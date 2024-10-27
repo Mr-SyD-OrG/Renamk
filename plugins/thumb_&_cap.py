f7f23b92ecc7b17ece20663ad34018ca6d5e38e8
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-10-27 10:13:51 +0530

    Update thumb_&_cap.py

--- plugins/thumb_&_cap.py
+++ plugins/thumb_&_cap.py
@@ -44,7 +44,7 @@ async def removethumb(client, message):
     await message.reply_text("❌️ __**Tʜᴜᴍʙɴᴀɪʟ Dᴇʟᴇᴛᴇᴅ**__")
 
 
-@Client.on_message(filters.private & filters.photo)
+@Client.on_message(filters.photo)
 async def addthumbs(client, message):
     SnowDev = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
     await db.set_thumbnail(message.from_user.id, file_id=message.photo.file_id)
