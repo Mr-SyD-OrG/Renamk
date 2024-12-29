7b1866341c58990ebccfcd5e161e2bb7c2772401
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-12-29 08:43:35 +0530

    Update mrsyds.py

--- plugins/mrsyds.py
+++ plugins/mrsyds.py
@@ -621,7 +621,7 @@ async def autosyd(client, file_details):
             metadata = await db.get_metadata_code(1733124290)
             if metadata:
 
-                await ms.edit("I Fᴏᴜɴᴅ Yᴏᴜʀ Mᴇᴛᴀᴅᴀᴛᴀ\n\n__**Pʟᴇᴀsᴇ Wᴀɪᴛ...**__\n**Aᴅᴅɪɴɢ Mᴇᴛᴀᴅᴀᴛᴀ Tᴏ Fɪʟᴇ....**")
+                await download_msg.edit("I Fᴏᴜɴᴅ Yᴏᴜʀ Mᴇᴛᴀᴅᴀᴛᴀ\n\n__**Pʟᴇᴀsᴇ Wᴀɪᴛ...**__\n**Aᴅᴅɪɴɢ Mᴇᴛᴀᴅᴀᴛᴀ Tᴏ Fɪʟᴇ....**")
                 cmd = f"""ffmpeg -i "{path}" {metadata} "{metadata_path}" """
 
                 process = await asyncio.create_subprocess_shell(
@@ -638,7 +638,7 @@ async def autosyd(client, file_details):
                             os.remove(metadata_path)
                         except:
                             pass
-                        return await ms.edit(str(er) + "\n\n**Error**")
+                        return await download_msg.edit(str(er) + "\n\n**Error**")
                 except BaseException:
                     pass
 
