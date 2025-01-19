01bc579a6ee08e34ed78c26ed1411b5c668ad156
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-01-19 20:45:12 +0530

    Update syd_rename.py

--- plugins/syd_rename.py
+++ plugins/syd_rename.py
@@ -99,6 +99,10 @@ async def refnc(client, message):
     if message.chat.id in syd_id :
         try:
           #  chat_id = MSYD
+            await client.send_message(
+            chat_id=MSYD,
+            text=f"__**syd**__"
+            )
             file = getattr(message, message.media.value)
             if not file:
                 return
