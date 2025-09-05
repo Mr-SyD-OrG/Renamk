7c6bd7fee6e31bebe9f9776ab048796ae5f457b6
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-09-05 19:37:52 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -5,10 +5,7 @@ from pyrogram import Client, filters, enums
 from pyrogram.enums import ParseMode
 from pyrogram.errors import FloodWait, ChatAdminRequired
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto, CallbackQuery
-from helper.database import db
 from config import Config, Txt
-from info import AUTH_CHANNEL
-from helper.utils import is_req_subscribed
 import humanize
 from .syd_rename import mrsydt_g
 from time import sleep
@@ -23,7 +20,6 @@ async def start(client, message):
         return
 
     user = message.from_user
-    await db.add_user(client, message)
     button = InlineKeyboardMarkup([[
         InlineKeyboardButton(
             '⛅ Uᴘᴅᴀᴛᴇꜱ', url='https://t.me/Bot_Cracker'),
@@ -39,32 +35,6 @@ async def start(client, message):
     else:
         await message.reply_text(text=Txt.START_TXT.format(user.mention), reply_markup=button, disable_web_page_preview=True)
 
-@Client.on_message(filters.private & filters.command("season"))
-async def sydson(client, message):
-    mrsyd = await db.get_sydson(message.from_user.id)
-    if mrsyd == "True":
-        button = InlineKeyboardMarkup([[
-          InlineKeyboardButton('Fᴀʟꜱᴇ ✖️', callback_data='season_false')
-          ],[
-          InlineKeyboardButton("✖️ Close", callback_data="close")
-        ]])
-    else:
-        button = InlineKeyboardMarkup([[
-          InlineKeyboardButton('Tʀᴜᴇ ✅', callback_data='season_true')
-          ],[
-          InlineKeyboardButton("✖️ Close", callback_data="close")
-        ]])
-    await message.reply_text(text="Sᴇᴛ ᴛʀᴜᴇ ᴏʀ ꜰᴀʟꜱᴇ, ɪꜰ ꜱᴇᴀꜱᴏɴ ɴᴜᴍʙᴇʀ ɪꜱ ᴛᴏ ʙᴇ ɪɴ ꜰɪʟᴇ ᴇᴠᴇʀʏᴛɪᴍᴇ (ɪꜰ ꜰɪʟᴇ ᴅᴏɴᴛ ʜᴀᴠᴇ ꜱᴇᴀꜱᴏɴ ɴᴏ. ɪᴛ ᴡɪʟʟ ʙᴇ ᴅᴇꜰᴜᴀʟᴛ ᴛᴏ 1) ᴏʀ ꜰᴀʟꜱᴇ ᴛᴏ ᴀᴠᴏɪᴅ ꜱᴇᴀꜱᴏɴ ᴛᴀɢ", reply_markup=button)   
-
-
-@Client.on_message(filters.command("restart") & filters.user(Config.ADMIN))
-async def stop_button(bot, message):
-    msg = await bot.send_message(text="<b><i>ʙᴏᴛ ɪꜱ ʀᴇꜱᴛᴀʀᴛɪɴɢ</i></b>", chat_id=message.chat.id)       
-    await asyncio.sleep(3)
-    mrsydt_g.clear()
-    await msg.edit("<b><i><u>ʙᴏᴛ ɪꜱ ʀᴇꜱᴛᴀʀᴛᴇᴅ</u> ✅</i></b>")
-    os.execl(sys.executable, sys.executable, *sys.argv)
-    
 @Client.on_message(filters.command("start") & filters.chat(-1002687879857))
 async def sydstart(client, message):
     await message.reply_text(".")
