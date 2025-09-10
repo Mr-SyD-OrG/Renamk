50f7017f04541a9f38588e5659adbeac69e2426b
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-09-10 07:21:06 +0530

    Update syd_rename.py

--- plugins/syd_rename.py
+++ plugins/syd_rename.py
@@ -15,7 +15,7 @@ import logging
 import re
 import os
 import time
-from helper.utils import add_prefix_suffix, client, start_clone_bot, is_req_subscribed
+from helper.utils import add_prefix_suffix, client, start_clone_bot #, is_req_subscribed
 from config import Config
 #from .mrsyds import mrsydtg
 from info import AUTH_CHANNEL
