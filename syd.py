f94b0d5268c73de8408e0a7d377d89d3c5b15c6e
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-09-10 07:16:34 +0530

    Update syd.py

--- syd.py
+++ syd.py
@@ -6,7 +6,7 @@ import re
 import os
 from helper.database import db
 from config import Config
-from info import AUTH_CHANNEL
+#from info import AUTH_CHANNEL
 from pyrogram.types import Message
 from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
 
@@ -16,12 +16,12 @@ logger.setLevel(logging.INFO)
 async def is_req_subscribed(bot, query):
     if await db.find_join_req(query.from_user.id):
         return True
-    try:
-        user = await bot.get_chat_member(AUTH_CHANNEL, query.from_user.id)
-    except UserNotParticipant:
-        pass
-    except Exception as e:
-        logger.exception(e)
+  #  try:
+      #  user = await bot.get_chat_member(AUTH_CHANNEL, query.from_user.id)
+    #except UserNotParticipant:
+       # pass
+   # except Exception as e:
+      #  logger.exception(e)
     else:
         if user.status != enums.ChatMemberStatus.BANNED:
             return True
